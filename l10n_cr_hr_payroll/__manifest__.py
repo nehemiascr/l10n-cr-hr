8fa8b56238755ca49230d796a807e2ba57b008cb
Nehemias Herrera <yo@nehemiascr.com>
2021-01-12 13:22:01 -0600

    [UPD] more rules, OCA folder organization

---
+++ l10n_cr_hr_payroll/__manifest__.py
@@ -0,0 +1,25 @@
+#-*- coding:utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Costa Rica Payroll',
+    'version': '10.0.1.0.1',
+    'category': 'Human Resources',
+    'author': 'Odoo CR Team, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'sumary': 'Costa Rica Payroll Rules',
+    'depends': [
+        'hr_payroll',
+    ],
+    'data': [
+        'data/hr_contribution_register_data.xml',
+        'data/hr_salary_rule_category_data.xml',
+        'data/hr_salary_rule_data.xml',
+        'data/hr_payroll_structure_data.xml',
+
+        'views/hr_payslip_line_views.xml',
+        'views/hr_payslip_run_views.xml',
+        'views/hr_payslip_views.xml',
+    ],
+}
